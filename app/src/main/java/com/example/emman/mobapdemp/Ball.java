ffd37a6e8df793cf4fd43e49c052152d808ee3cc
ER20 <38288335+ER20@users.noreply.github.com>
2018-12-08 13:47:43 +0800

    Moving ball on canvas

--- app/src/main/java/com/example/emman/mobapdemp/Ball.java
+++ app/src/main/java/com/example/emman/mobapdemp/Ball.java
@@ -1,6 +1,8 @@
 package com.example.emman.mobapdemp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -8,8 +10,15 @@ import android.graphics.Rect;
 import android.view.View;
 
 public class Ball  extends View {
+    Bitmap ball;
+    int x, y;
+
     public Ball(Context context) {
         super(context);
+
+        ball = BitmapFactory.decodeResource(getResources(), R.drawable.icon);
+        x = 0;
+        y = 0;
     }
 
     @Override
@@ -18,9 +27,27 @@ public class Ball  extends View {
         Rect ourRect = new Rect();
         ourRect.set(0, 0, canvas.getWidth(), canvas.getHeight()/2);
         Paint blue = new Paint();
-        blue.setColor(Color.BLUE);
+        blue.setColor(Color.WHITE);
         blue.setStyle(Paint.Style.FILL);
 
         canvas.drawRect(ourRect, blue);
+
+        if(x < canvas.getWidth()){
+            x += 10;
+        }
+        else{
+            x = 0;
+        }
+
+        if(y < canvas.getHeight()){
+            y += 10;
+        }
+        else{
+            y = 0;
+        }
+
+        Paint p = new Paint();
+        canvas.drawBitmap(ball, x, y, p);
+        invalidate();
     }
 }
